11e60c9a6f76a9e783339cfe21703fb8a4a92f97
Zuzanna Marchewka <marcheweczka007@gmial.com>
2020-11-19 20:00:28 +0000

    cleans up project

--- js/submitForm.js
+++ js/submitForm.js
@@ -7,11 +7,11 @@ function submitForm() {
    const email = document.getElementById('email').value;
    const mobile = document.getElementById('mobile').value;
 
-   if(!name || !email || !mobile) {
+   if (!name || !email || !mobile) {
       const message = document.getElementById('form-error');
       message.classList.add("visible");
       return
-   } 
+   }
 
    url = `formSubmitted.html?name=${encodeURIComponent(name)}&email=${encodeURIComponent(email)}&mobile=${encodeURIComponent(mobile)}`
    document.location.href = url;
